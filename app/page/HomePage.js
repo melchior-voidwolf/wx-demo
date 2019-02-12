4caa06b7dcc81f5824ec22dcf4532347020b9273
melchiorV <kmno4k2mno4@gmail.com>
2019-02-12 23:46:47 +0800

    添加收起和展开的样式处理

--- app/page/HomePage.js
+++ app/page/HomePage.js
@@ -3,28 +3,7 @@ import '@style/page/home-page.styl'
 
 class HomePage extends Component {
   render() {
-    const momentDemo = <div className="moment-timeline">
-      <div className="moment-timeline-block">
-        <div className="user-avator"></div>
-        <div className="moment-main">
-          <div className="user-name">我终于</div>
-          <div className="user-text-content">
-            稳扎稳打被憋我憋不出稳扎稳打被
-            憋我憋不出稳扎稳打被憋我憋不出稳扎稳
-            打被憋我憋不出稳扎稳打被憋我憋不出稳扎
-            稳打被憋我憋不出稳扎稳打被憋我憋不出稳扎
-            稳打被憋我憋不出稳扎稳打被憋我憋不出
-          </div>
-          <div className="user-action-line">
-            <div className="post-info">
-            3小时前
-              <span className="other-info">知乎APP</span>
-            </div>
-            <div className="enable-action-line-button">• •</div>
-          </div>
-        </div>
-      </div>
-    </div>
+
     return <div className='home-page'>
       <div className="user-background-wall">
         <div className="user-background-wall">
@@ -35,11 +14,49 @@ class HomePage extends Component {
           <div className="user-avator"></div>
         </div>
       </div>
-      { momentDemo }
-      { momentDemo }
-      { momentDemo }
-      { momentDemo }
-      { momentDemo }
+      <MomentTimeline />
+      <MomentTimeline />
+      <MomentTimeline />
+      <MomentTimeline />
+    </div>
+  }
+}
+
+class MomentTimeline extends Component {
+  state = {
+    displayAll: true
+  }
+  reveseContentDisplayStatus = () => {
+    this.setState({
+      displayAll: !this.state.displayAll
+    })
+  }
+  render() {
+    const { displayAll } = this.state
+    return <div className="moment-timeline">
+    <div className="moment-timeline-block">
+      <div className="user-avator"></div>
+      <div className="moment-main">
+        <div className="user-name">我终于</div>
+        <div className="text-content-wrapper">
+        <div className={"user-text-content" + (displayAll ? '' : ' user-text-content-hide')}>
+          稳扎稳打被憋我憋不出稳扎稳打被
+          憋我憋不出稳扎稳打被憋我憋不出稳扎稳
+          打被憋我憋不出稳扎稳打被憋我憋不出稳扎
+          稳打被憋我憋不出稳扎稳打被憋我憋不出稳扎
+          稳打被憋我憋不出稳扎稳打被憋我憋不出
+        </div>
+        </div>
+        <div className="content-control-button" onClick={this.reveseContentDisplayStatus}>{displayAll ? '收起' : '展开'}</div>
+        <div className="user-action-line">
+          <div className="post-info">
+          3小时前
+            <span className="other-info">知乎APP</span>
+          </div>
+          <div className="enable-action-line-button">• •</div>
+        </div>
+      </div>
+    </div>
     </div>
   }
 }
