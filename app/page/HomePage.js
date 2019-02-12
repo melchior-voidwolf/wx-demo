f1073c7dc00a78aac7c7bfbcba31281ef8a10e79
melchiorV <kmno4k2mno4@gmail.com>
2019-02-13 01:10:09 +0800

    添加可收起的状态栏

--- app/page/HomePage.js
+++ app/page/HomePage.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
 
 import MomentTimeline from '@component/MomentTimeline'
-
-import '@style/page/home-page.styl'
+import Titlebar from '@component/Titlebar'
 
 class HomePage extends Component {
+  state = {
+    titleOpacity: 0
+  }
+  titlebarStyleProcess = () => {
+    const {titleOpacity} = this.state
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+    if (scrollTop < 205) {
+      if (titleOpacity === 0) {
+        return null
+      } else {
+        this.setState({titleOpacity: 0})
+        return null
+      }
+    }
+    if (scrollTop > 275) {
+      if (titleOpacity === 1) {
+        return null
+      } else {
+        this.setState({titleOpacity: 1})
+        return null
+      }
+    }
+    this.setState({titleOpacity: (scrollTop - 205)/60 })
+  }
+  componentDidMount() {
+    window.addEventListener('scroll', this.titlebarStyleProcess)
+  }
+  componentWillMount() {
+    window.removeEventListener('scroll', this.titlebarStyleProcess)
+  }
   render() {
+    const {titleOpacity} = this.state
     return <div className='home-page'>
       <div className="user-background-wall">
         <div className="user-background-wall">
@@ -20,6 +50,7 @@ class HomePage extends Component {
       <MomentTimeline />
       <MomentTimeline />
       <MomentTimeline />
+      {titleOpacity!== 0 && <Titlebar style={{opacity: titleOpacity}} />}
     </div>
   }
 }
